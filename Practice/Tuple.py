b1bb58e235129a6a29912fbd3efc1beb0a054dab
foysal90 <foysal1416@hotmail.com>
2024-03-27 16:03:01 -0400

    range checked

--- Practice/Tuple.py
+++ Practice/Tuple.py
@@ -1,7 +1,10 @@
 items = ('Crispy Chicken', 'Tandoori', 'Chilli chicken')
-print(items[1])
+# checking ranges
+print('Ranges: ', items[1:3])
+
+# update
 cd = list(items)
 cd.append('Butter Chicken')
+cd[2] = 'CTM'
 btp = tuple(cd)
 print(btp)
-
