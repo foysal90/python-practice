a103f2b25931d852ba87a6e294b364a653777a3d
foysal90 <foysal1416@hotmail.com>
2024-03-28 01:56:43 -0400

    set method added

--- Practice/Tuple.py
+++ Practice/Tuple.py
@@ -4,7 +4,13 @@ print('Ranges: ', items[1:3])
 
 # update
 cd = list(items)
+# add item
 cd.append('Butter Chicken')
+# updating
 cd[2] = 'CTM'
+# remove item
+cd.remove('CTM')
 btp = tuple(cd)
-print(btp)
+
+for x in btp:
+    print('Access Items', x)
---
+++ Practice/set.py
@@ -0,0 +1,15 @@
+num1 = {5,2,5,4,3,6,9,7,8}
+num2 = {20,10,5,30,80,40,3}
+# set union
+print(num1 | num2)
+
+num1.add(888)
+print(num1)
+num1.discard(888)
+print(num1)
+# delete
+# del num1
+# print(num1)
+
+# set intersaction
+print('intersaction Result', num1 & num2)
